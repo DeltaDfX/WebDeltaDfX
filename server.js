b9787b0ef2d423d1f2e8d30147cdbef23fa119a0
TaiNguyen <minhtai306@gmail.com>
2019-10-14 15:50:16 +1100

    Update setting

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/WebAppCLI>'));
+app.use(express.static(__dirname + '/dist/WebDeltaDfX>'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/WebAppCLI/index.html'));
+
+res.sendFile(path.join(__dirname+'/dist/WebDeltaDfX/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
